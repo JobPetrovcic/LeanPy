d87c3b713d0960ac5e7a4be91d659ceb0cbecd5c
JobPetrovcic <job1.petrovcic@gmail.com>
2025-02-07 16:39:58 +0000

    removed old tests

--- tests/Kernel/test_Kernel.py
+++
@@ -1,9 +0,0 @@
-from LeanPy.Parsing.Lean4ExportParser import Lean4ExportParser
-from LeanPy.Structures.Expression.Expression import Const
-
-def test_decidable():
-    parsed = Lean4ExportParser.from_file("Exports/bool_reduction.export")
-    type_checker = parsed.type_checker
-    decl = type_checker.environment.get_declaration_under_name(string_to_name("A"))
-    
-    assert type_checker.def_eq(decl.value.arg, Const(type_checker.environment.Bool_true_name, []))
